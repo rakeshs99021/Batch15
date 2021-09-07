f676657f128c7394faf48a2c24ab1a24da007672
rakeshs99021 <87521748+rakeshs99021@users.noreply.github.com>
2021-09-07 10:44:20 +0530

    Add files via upload

---
+++ src/main/java/com/technoelvate/Controller/dto/Student.java
@@ -0,0 +1,15 @@
+package com.technoelvate.Controller.dto;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+public class Student {
+	private int id;
+	private String username;
+	private int password;
+	
+
+
+}
